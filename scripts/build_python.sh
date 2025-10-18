3a66be9819354addbc656204bc531b762a4227a5
Joe Banks <joe@jb3.dev>
2025-10-18 18:27:39 +0100

    Images should contain subdirectory for version
    This is to help for the libpath problem that snekbox is experiencing
    because the default library search path is changed when we copy files.

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -4,19 +4,29 @@ shopt -s inherit_errexit
 
 py_version="${1}"
 
+# Install Python interpreter under e.g. /snekbin/python/3.13/ (no patch version)
+# By dropping everything after, and including, the last period or hyphen.
+install_path="${py_version%[-.]*}"
+
+# Ensure the suffix letter is retained for free-threaded or JIT versions of Python.
+if [[ $py_version == *t ]]; then
+    install_path+="t"
+fi
+
 if [[ $py_version == *j ]]; then
     # Enable JIT mode when passed a version that ends with a "j"
+    install_path+="j"
     py_version="${py_version%j}"
     PYTHON_CONFIGURE_OPTS+=" --enable-experimental-jit"
 fi
 
 "${PYENV_ROOT}/plugins/python-build/bin/python-build" \
     "${py_version}" \
-    "/snekbin/"
-"/snekbin/bin/python" -m pip install -U pip
+    "/snekbin/python/${install_path}"
+"/snekbin/python/${install_path}/bin/python" -m pip install -U pip
 
 # Clean up some unnecessary files to reduce image size bloat.
-find /snekbin/ -depth \
+find /snekbin/python/ -depth \
 \( \
     \( -type d -a \( \
         -name test -o -name tests -o -name idle_test \
